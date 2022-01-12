451332b673d2747f222bcf0658e5806b174cbe2e
radoshDev <radosh.dev@gmail.com>
2022-01-12 16:49:59 +0200

    clear build

--- build/utils/calculateMonthSalary.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.calculateMonthSalary = void 0;
-function calculateMonthSalary(rows) {
-    return rows.reduce(function (sum, row) { return sum + Number(row.day_income); }, 0);
-}
-exports.calculateMonthSalary = calculateMonthSalary;
