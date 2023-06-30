67476fe6b5a07f493ff866e825a609b698f3d67c
Oleksandr Radoshniuk <radosh.dev@gmail.com>
2023-06-30 10:23:16 +0300

    remove build

--- build/utils/isAllowUser.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isAllowUser = void 0;
-const constants_1 = require("../constants");
-const isAllowUser = (userId) => {
-    return constants_1.ALLOWED_USER_LIST.includes(userId);
-};
-exports.isAllowUser = isAllowUser;
